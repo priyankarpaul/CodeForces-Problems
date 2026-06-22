bef26f7442b74ae2e0c9771f990ee80be6ef6939
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:06:35 +0530

    Add Passing the Ball [A] from Codeforces

---
+++ 2204/A - Passing the Ball/solution.cpp
@@ -0,0 +1,46 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long int
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+bool digit(ll n){
+    if(n%10!=0){
+        return true;
+    }
+    else return false;
+}
+ 
+void solve() {
+    
+    ll n;
+    cin>>n;
+    string s;
+    cin>>s;
+    for(int i=0;i<n;i++){
+        if(s[i]=='L'){
+            cout<<i+1<<"
+";
+            return;
+        }
+    }
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
