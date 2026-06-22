d3306d5840a49d892fcaa1535e83b34f9e398276
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:08:06 +0530

    Add Vasya and Socks [A] from Codeforces

---
+++ 460/A - Vasya and Socks/solution.cpp
@@ -0,0 +1,47 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long int
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+bool digit(ll n){
+    if(n%10!=0){
+        return true;
+    }
+    else return false;
+}
+ 
+ll lcmm(ll a,ll b){
+    return (a/__gcd(a,b))*b;
+}
+ 
+void solve() {
+    
+    ll n,m;
+    cin>>n>>m;
+ 
+    ll ans=n+((n-1)/(m-1));
+ 
+    cout<<ans<<"
+";
+ 
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
