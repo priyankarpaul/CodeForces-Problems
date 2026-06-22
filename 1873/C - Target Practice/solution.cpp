74f13fd10fce6e6138f91c6ef1f3e611220331e6
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:04:59 +0530

    Add Target Practice [C] from Codeforces

---
+++ 1873/C - Target Practice/solution.cpp
@@ -0,0 +1,56 @@
+/*Author SungJinWoo*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long int
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+const ll score[10][10]={
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,2,2,2,2,2,2,2,2,1},
+    {1,2,3,3,3,3,3,3,2,1},
+    {1,2,3,4,4,4,4,3,2,1},
+    {1,2,3,4,5,5,4,3,2,1},
+    {1,2,3,4,5,5,4,3,2,1},
+    {1,2,3,4,4,4,4,3,2,1},
+    {1,2,3,3,3,3,3,3,2,1},
+    {1,2,2,2,2,2,2,2,2,1},
+    {1,1,1,1,1,1,1,1,1,1}
+};
+void solve() {
+    char ch[10][10];
+    for(int i=0;i<10;i++){
+        string s;
+        cin>>s;
+        for(int j=0;j<10;j++){
+            ch[i][j]=s[j];
+        }
+    }
+    ll totalScore=0;
+    for(int i=0;i<10;i++){
+        for(int j=0;j<10;j++){
+            if(ch[i][j]=='X'){
+               totalScore+=score[i][j]; 
+            }
+        }
+    }
+    cout<<totalScore<<"
+";
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
