cd3a0ea5bb9ec0ae88fde422f7b19095f8237729
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:21:20 +0530

    Add Worms Evolution [A] from Codeforces

---
+++ 31/A - Worms Evolution/solution.cpp
@@ -0,0 +1,50 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long 
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+const ll maxX=1e6;
+ 
+ 
+void solve() {
+   
+   ll n;
+   cin>>n;
+   vector<ll> arr(n);
+   for(int i=0;i<n;i++) cin>>arr[i];
+ 
+   for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        for(int k=j+1;k<n;k++){
+            if(arr[i]==arr[j]+arr[k]){
+                cout<<i+1<<" "<<j+1<<" "<<k+1<<"
+";
+                return;
+            }
+        }
+    }
+   }
+   cout<<-1<<"
+";
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+ 
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
