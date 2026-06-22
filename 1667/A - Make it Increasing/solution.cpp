9325112645947850a9a2ba464a1dce8a5d8d84e3
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:26:27 +0530

    Add Make it Increasing [A] from Codeforces

---
+++ 1667/A - Make it Increasing/solution.cpp
@@ -0,0 +1,52 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long 
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+vector<ll> arr(5005); 
+void solve() {
+    
+  ll n;
+  cin>>n;
+  for(int i=1;i<=n;i++) cin>>arr[i];
+ 
+  ll ans=LLONG_MAX;
+ 
+  for(int i=1;i<=n;i++){
+    ll prev=0;
+    ll sum=0;
+    for(int j=i-1;j>=1;j--){
+        prev+=arr[j]-prev%arr[j];
+        sum+=prev/arr[j];
+    }
+    prev=0;
+    for(int k=i+1;k<=n;k++){
+        prev+=arr[k]-prev%arr[k];
+        sum+=prev/arr[k];
+    }
+    ans=min(ans,sum);
+  }
+  cout<<ans<<"
+";
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+ #ifndef ONLINE_JUDGE
+     freopen("input.txt",  "r",  stdin);
+     freopen("output.txt", "w", stdout);
+ #endif
+ 
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
