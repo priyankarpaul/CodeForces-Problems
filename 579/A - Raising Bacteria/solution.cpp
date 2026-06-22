78cd42ce67926124da73d59286cae62a69822b81
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:08:41 +0530

    Add Raising Bacteria [A] from Codeforces

---
+++ 579/A - Raising Bacteria/solution.cpp
@@ -0,0 +1,38 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long int
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+ 
+ 
+void solve() {
+  
+  ll a;
+  cin>>a;
+  ll count=0;
+  for(int i=0;i<32;i++){
+      if(((1<<i)&a)>0) count++;
+  }
+  cout<<count<<"
+";
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
