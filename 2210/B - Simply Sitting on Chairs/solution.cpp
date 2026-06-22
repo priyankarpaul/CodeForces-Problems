5749ba53e3e246b0f82dde23b4253b4e0363b826
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:10:18 +0530

    Add Simply Sitting on Chairs [B] from Codeforces

---
+++ 2210/B - Simply Sitting on Chairs/solution.cpp
@@ -0,0 +1,45 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long int
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+void solve() {
+     
+     ll n;
+     cin>>n;
+     vector<ll> arr(n);
+     for(int i=0;i<n;i++) cin>>arr[i];
+ 
+     vector<bool> visited(n,false);
+     ll count=0;
+     for(int i=0;i<n;i++){
+        if(visited[i]) break;
+        if(arr[i]-1<=i) {
+            count++;
+            visited[arr[i]-1]=true;
+        }
+     }
+     cout<<count<<"
+";
+ 
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
