f0bcb4a5f22686c6a2ce7e489c1c3850a165edea
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:06:59 +0530

    Add Comparison String [B] from Codeforces

---
+++ 1837/B - Comparison String/solution.cpp
@@ -0,0 +1,55 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long int
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+bool digit(ll n){
+    if(n%10!=0){
+        return true;
+    }
+    else return false;
+}
+ 
+ll lcmm(ll a,ll b){
+    return (a/__gcd(a,b))*b;
+}
+ 
+void solve() {
+    
+    ll n;
+    cin>>n;
+    ll count=1;
+    ll ans=1;
+    ll lessThanCount=0;
+    string s;
+    cin>>s;
+    for(int i=1;i<n;i++){
+        if(s[i]!=s[i-1]){
+            count=1;
+        }
+        else count++;
+        ans=max(ans,count);
+    }
+    cout<<ans+1<<"
+";
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
