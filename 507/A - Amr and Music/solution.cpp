656480d3bce61093af61410d64067e268d0a09a9
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:25:23 +0530

    Add Amr and Music [A] from Codeforces

---
+++ 507/A - Amr and Music/solution.cpp
@@ -0,0 +1,56 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long 
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+ 
+void solve() {
+    
+    ll n,days;
+    cin>>n>>days;
+ 
+    vector<pair<ll,ll>> arr(n);
+    for(ll i=0;i<n;i++){
+        cin>>arr[i].ff;
+        arr[i].ss=i;
+    }
+    
+    vector<ll> ans;
+ 
+    sort(arr.begin(),arr.end());
+    for(int i=0;i<n;i++){
+        if(days<arr[i].ff) break;
+        ans.pb(arr[i].ss+1);
+        days-=arr[i].ff;
+    }
+ 
+    sort(ans.begin(),ans.end());
+    cout<<ans.size()<<"
+";
+    for(ll i=0;i<ans.size();i++){
+        if(i) cout<<" ";
+        cout<<ans[i];
+    }
+ 
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+ #ifndef ONLINE_JUDGE
+     freopen("input.txt",  "r",  stdin);
+     freopen("output.txt", "w", stdout);
+ #endif
+ 
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
