376b55ef1d5989d5bfc7085c3a54565b9df53f9f
Priyankar Paul <godthorg@gmail.com>
2026-06-22 05:59:02 +0530

    Add Young Physicist [A] from Codeforces

---
+++ 69/A - Young Physicist/solution.cpp
@@ -0,0 +1,55 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll            long long int
+#define ff              first
+#define ss              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define endl           "
+"
+#define double         long double
+ 
+const ll MOD = 1e9 + 7;
+ 
+void solve() {
+   
+   ll n;cin>>n;
+   ll sum1=0,sum2=0,sum3=0;
+   while(n--){
+       ll x,y,z;
+       cin>>x>>y>>z;
+       sum1+=x;
+       sum2+=y;
+       sum3+=z;
+   }
+   if(sum1==0&&sum1==0&&sum2==0) cout<<"YES
+";
+   else cout<<"NO
+";
+ 
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
