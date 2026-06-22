ca85abaa8a22fd886b7af7a892c3fe709f23120f
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:27:20 +0530

    Add Tavas and Nafas [A] from Codeforces

---
+++ 535/A - Tavas and Nafas/solution.cpp
@@ -0,0 +1,109 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long 
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+ 
+ll lowerBound(vector<ll>& arr,ll n,ll target){
+    ll low=0;
+    ll high=n-1;
+    ll ans=n;
+    while(low<=high){
+        ll mid=low+(high-low)/2;
+        if(arr[mid]>=target){
+            ans=min(ans,mid);
+            high=mid-1;
+        }
+        else low=mid+1;
+    }
+    return ans;
+}
+ 
+ll upperBound(vector<ll>& arr,ll n,ll target){
+    ll low=0;
+    ll high=n-1;
+    ll ans=n;
+    while(low<=high){
+        ll mid=low+(high-low)/2;
+        if(arr[mid]>target){
+            ans=min(ans,mid);
+            high=mid-1;
+        }
+        else low=mid+1;
+    }
+    return ans;
+}
+ 
+void solve() {
+ 
+   ll n;
+   cin>>n;
+   ll left,right;
+   string str1[]={  
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen"
+                 };
+ 
+    string str2[]={
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety"
+                 };  
+ 
+    if(n==0) cout<<"zero
+";
+    else if(n<=19) cout<<str1[n-1]<<"
+";
+    else{
+      left=n/10;
+      right=n%10;
+      if(right==0) cout<<str2[left-2]<<"
+";
+      else cout<<str2[left-2]<<"-"<<str1[right-1]<<"
+";
+    }
+ 
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+ #ifndef ONLINE_JUDGE
+     freopen("input.txt",  "r",  stdin);
+     freopen("output.txt", "w", stdout);
+ #endif
+ 
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
