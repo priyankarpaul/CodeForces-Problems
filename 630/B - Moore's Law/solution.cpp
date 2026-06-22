c9d3f8fd83eb5484d170048d78ef7e0900b01405
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:29:04 +0530

    Add Moore's Law [B] from Codeforces

---
+++ 630/B - Moore's Law/solution.cpp
@@ -0,0 +1,35 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long 
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+void solve() {
+ 
+  long double n,t;
+  cin>>n>>t;
+  long double ans=pow(1.000000011,t);
+ 
+  cout<<fixed<<setprecision(7)<<n*ans<<"
+";
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+ #ifndef ONLINE_JUDGE
+     freopen("input.txt",  "r",  stdin);
+     freopen("output.txt", "w", stdout);
+ #endif
+ 
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
