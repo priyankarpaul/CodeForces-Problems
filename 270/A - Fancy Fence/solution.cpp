74f590f1e2c1a753f9a0cf513b5c0dfb4040faaf
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:26:15 +0530

    Add Fancy Fence [A] from Codeforces

---
+++ 270/A - Fancy Fence/solution.cpp
@@ -0,0 +1,40 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long 
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+ 
+void solve() {
+    
+  
+  ll a;
+  cin>>a;
+  if(360%(180-a)==0){
+    cout<<"YES
+";
+  }
+  else cout<<"NO
+";
+  
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+ #ifndef ONLINE_JUDGE
+     freopen("input.txt",  "r",  stdin);
+     freopen("output.txt", "w", stdout);
+ #endif
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
