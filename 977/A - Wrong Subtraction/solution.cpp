035d3697366a885c83f608f3dc6487da184d0a9c
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:05:48 +0530

    Add Wrong Subtraction [A] from Codeforces

---
+++ 977/A - Wrong Subtraction/solution.cpp
@@ -0,0 +1,44 @@
+/*Author SungJinWoo*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long int
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+bool digit(ll n){
+    if(n%10!=0){
+        return true;
+    }
+    else return false;
+}
+ 
+void solve() {
+    ll n,k;
+    cin>>n>>k;
+    for(int i=0;i<k;i++){
+        if(digit(n)){
+            n=n-1;
+        }
+        else n/=10;
+    }
+    cout<<n<<"
+";
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+   // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
