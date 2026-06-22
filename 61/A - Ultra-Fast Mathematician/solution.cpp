c6da4101d69f3c3907d06d7d04fd5a2325b55ead
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:15:30 +0530

    Add Ultra-Fast Mathematician [A] from Codeforces

---
+++ 61/A - Ultra-Fast Mathematician/solution.cpp
@@ -0,0 +1,40 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long int
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+void solve() {
+   
+   string s1,s2;
+   cin>>s1>>s2;
+ 
+   for(int i=0;i<s1.size();i++){
+       if(s1[i]==s2[i]){
+        cout<<0;
+       }
+       if(s1[i]!=s2[i]){
+        cout<<1;
+       }
+   }
+ 
+} 
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
