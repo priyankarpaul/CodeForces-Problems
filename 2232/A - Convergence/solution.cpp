4fdf443db8a690bd7fda5a7623fac25978f07e8c
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:18:55 +0530

    Add Convergence [A] from Codeforces

---
+++ 2232/A - Convergence/solution.cpp
@@ -0,0 +1,49 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long int
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+int gcd(ll a,ll b ){
+    if (b==0){
+        return a;
+    }
+    else{
+        return gcd(a,a%b);
+    }
+}
+void solve() {  
+    
+    ll n;
+    cin>>n;
+ 
+    vector<ll> arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+ 
+    ll count=0;
+    sort(arr.begin(),arr.end());
+ 
+    for(int i=0;i<n;i++){
+        if(arr[i]!=arr[n-i-1]) count++;
+    }
+    cout<<count/2<<"
+";
+} 
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
