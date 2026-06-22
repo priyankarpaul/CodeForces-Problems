b89053fa6fa3a211151ff045a414c6db5b2feb2a
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:14:20 +0530

    Add Exciting Bets [A] from Codeforces

---
+++ 1543/A - Exciting Bets/solution.cpp
@@ -0,0 +1,41 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long int
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+void solve() {
+   
+   ll a,b;
+   cin>>a>>b;
+   if(a==b){
+    cout<<0<<" "<<0<<"
+";
+   }
+   else{
+   ll g=abs(a-b);
+   ll x=min(a%g,g-a%g);
+   cout<<g<<" "<<x<<"
+";
+   }
+ 
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
