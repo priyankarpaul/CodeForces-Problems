2e5d0b6d67c9541170709c4138e381cc606c5fae
Priyankar Paul <godthorg@gmail.com>
2026-06-22 05:58:38 +0530

    Add Bit++ [A] from Codeforces

---
+++ 282/A - Bit++/solution.cpp
@@ -0,0 +1,53 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll            long long int
+#define ff              first
+#define ss              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define endl           "
+"
+#define double         long double
+ 
+const ll MOD = 1e9 + 7;
+ 
+void solve() {
+    ll value=0;
+    ll n;cin>>n;
+    while(n--){
+       string s;
+       cin>>s;
+       ll n=s.size();
+        for(int i=0;i<n;i++){
+            if(s[i]=='+'&&s[i+1]=='+') value++;
+            else if(s[i]=='-'&&s[i+1]=='-') value--;
+        }
+    }
+     cout<<value<<"
+";
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
