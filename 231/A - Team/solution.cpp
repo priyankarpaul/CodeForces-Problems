78069b9ed4a8ac5a4313887106cda5d7486bab48
Priyankar Paul <godthorg@gmail.com>
2026-06-22 05:58:29 +0530

    Add Team [A] from Codeforces

---
+++ 231/A - Team/solution.cpp
@@ -0,0 +1,52 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll            long long int
+#define ff              first
+#define ss              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define endl           "
+"
+#define double         long double
+ 
+const ll MOD = 1e9 + 7;
+ 
+void solve() {
+    
+    ll n;
+    cin>>n;
+    ll count=0;
+    while(n--){
+       ll a,b,c;
+       cin>>a>>b>>c;
+       if(a+b+c>=2) count++;
+    }
+    cout<<count<<"
+";
+     
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
