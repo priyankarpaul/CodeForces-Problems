13758d009e738195fbb4ea7a71b0db87daf24edb
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:09:18 +0530

    Add Bear and Big Brother [A] from Codeforces

---
+++ 791/A - Bear and Big Brother/solution.cpp
@@ -0,0 +1,40 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long int
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+ 
+ 
+void solve() {
+  
+  ll a,b;
+  cin>>a>>b;
+  ll count=0;
+  while(a<=b){
+    a*=3;
+    b*=2;
+    count++;
+  }
+  cout<<count<<"
+";
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
