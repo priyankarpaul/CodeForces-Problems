99a8946c5b863049fb02d4ad7bfdadab198efa12
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:18:47 +0530

    Add Lucky Year [A] from Codeforces

---
+++ 808/A - Lucky Year/solution.cpp
@@ -0,0 +1,38 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long int
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+void solve() {  
+    
+    ll n;
+    cin>>n;
+    ll mag=1;
+    while(mag*10<=n){
+        mag*=10;
+    }
+    ll firstDigit=n/mag;
+    ll next=(firstDigit+1)*mag;
+    cout<<next-n<<"
+";
+} 
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
