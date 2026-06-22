c4c51e29ba785c28b84d06df51a40ed08e2ce4d0
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:01:31 +0530

    Add Even Odds [A] from Codeforces

---
+++ 318/A - Even Odds/solution.cpp
@@ -0,0 +1,60 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll            long long int
+#define ff              first
+#define ss              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define endl           "
+"
+#define double          double
+ 
+const ll MOD = 1e9 + 7;
+ 
+ll digit(ll n){
+    ll sum=0;
+    while(n!=0){
+        ll remainder=n%10;
+        sum=sum*10+remainder;
+        n/=10;
+    }
+    return sum;
+}
+ 
+void solve() {
+   ll n;
+   cin>>n;
+   ll k;
+   cin>>k;
+   ll oddCount=(n+1)/2;
+ 
+   if(k<=oddCount) cout<<2*k-1<<"
+";
+   else cout<<2*(k-oddCount)<<"
+";
+ 
+} 
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
