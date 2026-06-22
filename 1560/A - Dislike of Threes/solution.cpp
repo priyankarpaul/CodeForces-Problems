c2c7003de28a8ea9be3a80b78608f8cb9433467e
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:00:53 +0530

    Add Dislike of Threes [A] from Codeforces

---
+++ 1560/A - Dislike of Threes/solution.cpp
@@ -0,0 +1,48 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll            long long int
+#define ff              first
+#define ss              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define endl           "
+"
+#define double          double
+ 
+const ll MOD = 1e9 + 7;
+ 
+void solve() {
+    vector<ll> arr;
+    for(int i=1;i<=100100;i++){
+        if(i%3==0||i%10==3) continue;
+        else arr.pb(i);
+    }
+     ll n;
+     cin>>n;
+     cout<<arr[n-1]<<"
+";
+} 
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
