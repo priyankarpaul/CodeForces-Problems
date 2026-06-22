f6a7b8b7cc0898110d40f9a79115a393a1c0f589
Priyankar Paul <godthorg@gmail.com>
2026-06-22 05:59:27 +0530

    Add Specialty String [C] from Codeforces

---
+++ 2200/C - Specialty String/solution.cpp
@@ -0,0 +1,54 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll            long long int
+#define ff              first
+#define ss              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define endl           "
+"
+#define double         long double
+ 
+const ll MOD = 1e9 + 7;
+ 
+void solve() {
+   
+   ll n;
+   cin>>n;
+   string s;
+   cin>>s;
+   stack<ll> st;
+   for(int i=0;i<n;i++){
+    if(st.size()&&s[i]==st.top()) st.pop();
+    else st.push(s[i]);
+   }
+   if(st.empty()) cout<<"YES
+";
+   else cout<<"NO
+";
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
