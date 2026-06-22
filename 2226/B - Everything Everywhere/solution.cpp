e327afcee76d3dff226f1d4218940ee692998326
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:15:53 +0530

    Add Everything Everywhere [B] from Codeforces

---
+++ 2226/B - Everything Everywhere/solution.cpp
@@ -0,0 +1,41 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long int
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+void solve() {
+   
+     ll n;
+     cin>>n;
+     vector<ll> arr(n);
+     for(int i=0;i<n;i++) cin>>arr[i];
+ 
+     ll count=0;
+     for(int i=0;i<n-1;i++){
+        ll difference=abs(arr[i]-arr[i+1]);
+        if(difference==__gcd(arr[i],arr[i+1])) count++;
+     }
+ 
+     cout<<count<<"
+";
+} 
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
