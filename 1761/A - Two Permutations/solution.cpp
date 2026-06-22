e4a6db988a0402424773f93fc6d88513b9d48d93
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:19:45 +0530

    Add Two Permutations [A] from Codeforces

---
+++ 1761/A - Two Permutations/solution.cpp
@@ -0,0 +1,37 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long int
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+void solve() {  
+    
+    ll n,a,b;
+    cin>>n>>a>>b;
+    
+    if(((a+b+2)<=n)||((a==b)&&(b==n)&&(a==n))){
+        cout<<"Yes
+";
+    }
+    else cout<<"No
+";
+} 
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
