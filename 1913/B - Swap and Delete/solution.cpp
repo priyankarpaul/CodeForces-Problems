4da1975f30a5aa83fe895c4c264650524ba7b8e0
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:08:11 +0530

    Add Swap and Delete [B] from Codeforces

---
+++ 1913/B - Swap and Delete/solution.cpp
@@ -0,0 +1,65 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long int
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+bool digit(ll n){
+    if(n%10!=0){
+        return true;
+    }
+    else return false;
+}
+ 
+ll lcmm(ll a,ll b){
+    return (a/__gcd(a,b))*b;
+}
+ 
+void solve() {
+    
+    string s;cin>>s;
+    ll n=s.size();
+ 
+    ll oneCount=0;
+    ll zeroCount=0;
+ 
+    for(int i=0;i<n;i++){
+        if(s[i]=='1'){
+            oneCount++;
+        }
+        else zeroCount++;
+    }
+    ll length=0;
+    for(int i=0;i<n;i++){
+        if(s[i]=='0'&&oneCount>0){
+            oneCount--;
+            length++;
+        }
+        else if(s[i]=='1'&&zeroCount>0){
+            zeroCount--;
+            length++;
+        }
+        else break;
+    }
+    cout<<n-length<<"
+";
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
