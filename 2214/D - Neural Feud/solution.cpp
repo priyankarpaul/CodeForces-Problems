df9fed3d47b61c53fcb0e7d6fea28fe7ec396551
Priyankar Paul <godthorg@gmail.com>
2026-06-22 06:11:32 +0530

    Add Neural Feud [D] from Codeforces

---
+++ 2214/D - Neural Feud/solution.cpp
@@ -0,0 +1,48 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long int
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+void solve() {
+ 
+    ll n;
+    cin>>n;
+    if(n==1) cout<<"Walk
+";
+    if(n==2) cout<<"No
+";
+    if(n==3) cout<<"No
+";
+    if(n==4) cout<<"No
+";
+    if(n==5) cout<<"Yes
+";
+    if(n==6) cout<<"Yes
+";
+    if(n==7) cout<<"backwards
+";
+    if(n==8) cout<<"seven
+";
+ 
+ 
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
