04cef51033b961759f4e11e7ef51bf6e130299af
Priyankar Paul <godthorg@gmail.com>
2026-06-24 07:45:41 +0530

    Add Ksusha and Array [A] from Codeforces

---
+++ 299/A - Ksusha and Array/solution.cpp
@@ -0,0 +1,50 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long 
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+ 
+void solve() {
+ 
+  ll n;
+  cin>>n;
+  vector<ll> arr(n);
+  ll mini=LLONG_MAX;
+  for(int i=0;i<n;i++){ 
+    cin>>arr[i];
+    mini=min(arr[i],mini);
+  }
+ 
+  for(int i=0;i<n;i++){
+    if(arr[i]%mini!=0){
+        cout<<-1<<"
+";
+        return;
+    }
+  }
+  
+  cout<<mini<<"
+";
+ 
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+     freopen("input.txt",  "r",  stdin);
+     freopen("output.txt", "w", stdout);
+#endif
+ 
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
