746f0509334828a3751b290ad9f9298cceac3453
Priyankar Paul <godthorg@gmail.com>
2026-06-24 08:07:44 +0530

    Add Candy Bags [A] from Codeforces

---
+++ 334/A - Candy Bags/solution.cpp
@@ -0,0 +1,45 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long 
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+ 
+void solve() {
+ 
+  ll n;
+  cin>>n;
+  ll left=1;
+  ll right=n*n;
+ 
+  for(int i=1;i<=n;i++){
+     for(int j=0;j<n/2;j++){
+        cout<<left<<" "<<right<<" ";
+        left++;
+        right--;   
+      }
+      cout<<"
+";
+  }
+ 
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+     freopen("input.txt",  "r",  stdin);
+     freopen("output.txt", "w", stdout);
+#endif
+ 
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
