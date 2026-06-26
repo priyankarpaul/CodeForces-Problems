aa634991c897b1ceb69fe75b089d624e9f75aa9d
Priyankar Paul <godthorg@gmail.com>
2026-06-26 11:01:49 +0530

    Add Shovels and Swords [A] from Codeforces

---
+++ 1366/A - Shovels and Swords/solution.cpp
@@ -0,0 +1,38 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long 
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+ 
+void solve() {
+ 
+  ll a,b;
+  cin>>a>>b;
+   
+  ll ans=min({a,b,(a+b)/3});
+  cout<<ans<<"
+";
+ 
+  
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+     freopen("input.txt",  "r",  stdin);
+     freopen("output.txt", "w", stdout);
+#endif
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
