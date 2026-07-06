253cd393bedf4d49761ba1d498922849da6d2a3a
Priyankar Paul <godthorg@gmail.com>
2026-07-06 21:02:24 +0530

    Add Game [A] from Codeforces

---
+++ 984/A - Game/solution.cpp
@@ -0,0 +1,37 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long 
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+ 
+void solve() {
+    
+    ll n;
+    cin>>n;
+    vector<ll> arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    sort(arr.begin(),arr.end());
+    cout<<arr[(n-1)/2]<<"
+";
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+     freopen("input.txt",  "r",  stdin);
+     freopen("output.txt", "w", stdout);
+#endif
+ 
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
