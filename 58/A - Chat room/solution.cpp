89ad6e6bd643c5fb99952477ddfeeac2270e528d
Priyankar Paul <godthorg@gmail.com>
2026-07-13 23:35:56 +0530

    Add Chat room [A] from Codeforces

--- 58/A - Chat room/solution.cpp
+++ 58/A - Chat room/solution.cpp
@@ -3,36 +3,25 @@
 #include <bits/stdc++.h>
 using namespace std;
  
-#define ll            long long int
+#define ll              long long 
 #define ff              first
 #define ss              second
-#define pb             push_back
-#define si             set <int>
-#define vi             vector <int>
-#define pii            pair <int, int>
-#define vpi            vector <pii>
-#define vpp            vector <pair<int, pii>>
-#define mii            map <int, int>
-#define mpi            map <pii, int>
-#define spi            set <pii>
-#define endl           "
-"
-#define double         long double
- 
+#define pb              push_back
 const ll MOD = 1e9 + 7;
  
+ 
 void solve() {
-   
-   string s;
+    
+     string s;
    cin>>s;
    ll n=s.size();
- 
+ 
    ll h=1;
    ll e=1;
    ll LL=2;
    ll o=1;
    ll count=0;
- 
+ 
    for(int i=0;i<n;i++){
        if(s[i]=='h'&&h==1&&e==1&&LL==2&&o==1){
         h--;
@@ -55,21 +44,15 @@ void solve() {
         count++;
        }
    }
- 
+ 
    if(count>=5) cout<<"YES
 ";
    else cout<<"NO
 ";
-   
+    
 }
- 
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
- 
-#ifndef ONLINE_JUDGE
-    freopen("input.txt",  "r",  stdin);
-    freopen("output.txt", "w", stdout);
-#endif
     int t = 1;
     //cin >> t;
     while (t--) {
