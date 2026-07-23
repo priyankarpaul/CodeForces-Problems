abf1724d6282e43c1d314d49311fe271813b1a9c
Priyankar Paul <godthorg@gmail.com>
2026-07-23 23:06:01 +0530

    Add Put Knight! [E] from Codeforces

---
+++ 120/E - Put Knight!/solution.cpp
@@ -0,0 +1,33 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long 
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9 + 7;
+ 
+ 
+void solve() {
+    
+    ll n;
+    cin>>n;
+ 
+    if(n%2!=0) cout<<0<<"
+";
+    else cout<<1<<"
+";
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
