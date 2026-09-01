5da96102b0e87322c35c260ec2c662c471dacd9d
Priyankar Paul <godthorg@gmail.com>
2026-09-01 21:14:21 +0530

    Add Maximum GCD [A] from Codeforces

---
+++ 1370/A - Maximum GCD/solution.cpp
@@ -0,0 +1,31 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long 
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9+7;
+ 
+ll gc(ll n,ll m){
+     return __gcd(n,m);
+}
+ 
+void solve() {
+    
+    ll n;
+    cin>>n;
+    cout<<n/2<<"
+";
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
