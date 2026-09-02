66ad4e428cea0f8591787024df11ce8795ec56f1
Priyankar Paul <godthorg@gmail.com>
2026-09-03 04:45:30 +0530

    Add The World is a Theatre [C] from Codeforces

---
+++ 131/C - The World is a Theatre/solution.cpp
@@ -0,0 +1,57 @@
+/*Author SungJinWoo18*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll              long long 
+#define ff              first
+#define ss              second
+#define pb              push_back
+const ll MOD = 1e9+7;
+ 
+bool prime(ll sum){
+    if(sum<2) return false;
+    if(sum==3||sum==2) return true;
+    if(sum%2==0||sum%3==0) return false;
+    for(ll i=5;i*i<=sum;i+=6){
+       if(sum%i==0||sum%(i+2)==0) return false;
+    }
+   return true;
+}
+ 
+ll combination(ll n,ll k){
+    if(k<0||k>n) return 0;
+    if(k==0||k==n) return 1;
+    if(k>n/2)k=n-k;
+    ll res=1;
+    for(ll i=1;i<=k;i++){
+        res=res*(n-i+1)/i;
+    }
+    return res;
+}
+ 
+void solve() {
+    
+    ll n,m,t;
+    cin>>n>>m>>t;
+    
+    ll totalWays=0;
+    for(ll i=4;i<= n;i++){
+        ll g=t-i;
+        if(g>=1&&g<=m){
+            totalWays+=combination(n,i)*combination(m,g);
+        }
+    }
+    cout<<totalWays<<"
+";
+ 
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t=1;
+    //cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
