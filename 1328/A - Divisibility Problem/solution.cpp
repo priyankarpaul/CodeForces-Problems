bfff728ac810643b8875f7b537f3788f768083fc
Priyankar Paul <godthorg@gmail.com>
2026-09-10 10:37:00 +0530

    Add Divisibility Problem [A] from Codeforces

--- 1328/A - Divisibility Problem/solution.cpp
+++ 1328/A - Divisibility Problem/solution.cpp
@@ -3,14 +3,15 @@
 #include <bits/stdc++.h>
 using namespace std;
  
-#define ll              long long int
+#define ll              long long 
 #define ff              first
 #define ss              second
 #define pb              push_back
 const ll MOD = 1e9 + 7;
  
+ 
 void solve() {
-   
+    
    ll a,b;
    cin>>a>>b;
    ll count=0;
@@ -18,14 +19,9 @@ void solve() {
 ";
    else cout<<b-a%b<<"
 ";
-} 
+}
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
- 
-#ifndef ONLINE_JUDGE
-    freopen("input.txt",  "r",  stdin);
-    freopen("output.txt", "w", stdout);
-#endif
     int t = 1;
     cin >> t;
     while (t--) {
